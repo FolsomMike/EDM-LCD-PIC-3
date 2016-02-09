e762b07634b72645958625f8815f9d99bb5a02d2
Mike Schoonover <mike@skoonie.com>
2016-02-08 18:12:43 -0600

    Removed IDE project files after adding them to gitignore. These files are constantly regenerated and shouldn't be tracked.
    Signed-off-by: Mike Schoonover <mike@skoonie.com>

--- build/default/production/OPT EDM LCD PIC.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/OPT\ EDM\ LCD\ PIC.o:  \
-	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects/OPT-EDM-LCD-PIC-3/OPT\ EDM\ LCD\ PIC.asm  \
-	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects/OPT-EDM-LCD-PIC-3/P16F648A.INC  \
-	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects/OPT-EDM-LCD-PIC-3/standard.mac  \
-
